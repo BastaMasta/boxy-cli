006861937f83d722e3412ad458fce774e7ff1c2f
BastaMasta <56192822+BastaMasta@users.noreply.github.com>
2024-10-30 11:57:50 +0000

    Added boilerplate code to the library project

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,22 @@
+mod defs;
+
+use crate::defs::*;
+
+
 pub fn add(left: u64, right: u64) -> u64 {
+    println!("{:?}", SINGLE_TEMPLATE);
     left + right
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
+
+//     #[test]
+//     fn it_works() {
+//         let result = add(2, 2);
+//         assert_eq!(result, 4);
+//     }
+// }
