b803611467715503ec700896c05b913577000a8d
BastaMasta <56192822+BastaMasta@users.noreply.github.com>
2024-10-30 01:56:07 +0000

    Incremental commit #1

--- binary-testing/src/main.rs
+++ binary-testing/src/main.rs
@@ -1,28 +1,52 @@
 use colored::Colorize;
 
 fn main() {
-    boxy();
+    boxy(" Hello There BOIII");
 }
 
 
-fn boxy() {
+fn boxy(data: &str) {
+    let terminal_size = 20;
+    let whitespace_indices_temp = data.trim().match_indices(" ").collect::<Vec<_>>();
+    let mut ws_indices = Vec::new();
+    for (i,_) in whitespace_indices_temp {
+        ws_indices.push(i);
+    }
+    println!("{:?}", nearest_whitespace(&mut ws_indices, &terminal_size, 0));
+    println!("{:?}", ws_indices);
     let light = ["┌", "┐", "└", "┘", "│", "─", "├", "┼", "┤"];
+    let mut padded_string = String::from(" ");
     print!("{}", light[0].blue());
-    for _ in 0..50 {
+    for _ in 0..=terminal_size {
         print!("{}", light[5].blue());
     }
     println!("{}", light[1].blue());
     for __ in 0..5 {
         print!("{}", light[4].blue());
-        for _ in 0..50 {
-            print!(" ") 
+        for _ in 0..=terminal_size {
+            print!(" "); 
         };
         print!("{}", light[4].blue());
         println!("");
     }
     print!("{}", light[4].blue());
-    print!("12345678901234567890123456789012345678901234567890");
+    padded_string = format!("{:<20}", &data.trim()[0..nearest_whitespace(&mut ws_indices, &terminal_size, 0)]);
+    print!("{}", padded_string);
     print!("{}", light[4].blue());
     println!("");
 
+}
+
+fn nearest_whitespace(map: &mut Vec<usize>, term_size: &usize, start_index: usize) -> usize {
+    let mut prev = 0;
+    let mut curr = 0;
+    for i in &mut map[start_index..] {
+        curr = *i;
+        if curr > (*term_size) {
+            return prev;
+        } else {
+            prev = curr;
+        }
+    }
+    return curr;
 }
