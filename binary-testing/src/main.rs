ca48374e3d60109ad079b34b7ee7582d3f0879c5
BastaMasta <56192822+BastaMasta@users.noreply.github.com>
2024-10-30 10:36:58 +0000

    Added color support

--- binary-testing/src/main.rs
+++ binary-testing/src/main.rs
@@ -1,4 +1,5 @@
 use colored::Colorize;
+use hex_color::HexColor;
 
 // Index 0 for full light
 // Index 1 for double horizonta and light vertical
@@ -11,13 +12,14 @@ const PEICES : [[&str; 11]; 4] = [["┌", "┐", "└", "┘", "│", "─", "�
                                   ["╔", "╗", "╚", "╝", "║", "═", "╠", "╬", "╣", "╦", "╩"]];
 
 fn main() {
-    boxy(" Hello There BOIII");
+    boxy(" Hello There BOIII", "#DDA0DD80");
 }
 
 
-fn boxy(data: &str) {
+fn boxy(data: &str, color : &str) {
     let term = termsize::get().unwrap();
     let terminal_size = (term.cols as usize) - 20;
+    let col_truevals = HexColor::parse(color).unwrap();
     let mut processed_data = String::from (data.trim());
     processed_data.push(' ');
     let whitespace_indices_temp = processed_data.match_indices(" ").collect::<Vec<_>>();
@@ -27,23 +29,23 @@ fn boxy(data: &str) {
     }
     println!("{:?}", nearest_whitespace(&mut ws_indices, &terminal_size, 0));
     println!("{:?}", ws_indices);
-    print!("{}", PEICES[0][0].blue());
+    print!("{}", PEICES[0][0].truecolor(col_truevals.r, col_truevals.g, col_truevals.b));
     for _ in 0..=terminal_size {
-        print!("{}", PEICES[0][5].blue());
+        print!("{}", PEICES[0][5].truecolor(col_truevals.r, col_truevals.g, col_truevals.b));
     }
-    println!("{}", PEICES[0][1].blue());
-    print!("{}", PEICES[0][4].blue());
+    println!("{}", PEICES[0][1].truecolor(col_truevals.r, col_truevals.g, col_truevals.b));
+    print!("{}", PEICES[0][4].truecolor(col_truevals.r, col_truevals.g, col_truevals.b));
     let padded_string = format!("   {:<width$}", 
                                 &processed_data[0..nearest_whitespace(&mut ws_indices, &(&terminal_size-5), 0)],
                                 width=terminal_size-2);
     print!("{}", padded_string);
-    print!("{}", PEICES[0][4].blue());
+    print!("{}", PEICES[0][4].truecolor(col_truevals.r, col_truevals.g, col_truevals.b));
     println!("");
-    print!("{}", PEICES[0][2].blue());
+    print!("{}", PEICES[0][2].truecolor(col_truevals.r, col_truevals.g, col_truevals.b));
     for _ in 0..=terminal_size {
-        print!("{}", PEICES[0][5].blue());
+        print!("{}", PEICES[0][5].truecolor(col_truevals.r, col_truevals.g, col_truevals.b));
     }
-    println!("{}", PEICES[0][3].blue());
+    println!("{}", PEICES[0][3].truecolor(col_truevals.r, col_truevals.g, col_truevals.b));
     
 
 }
