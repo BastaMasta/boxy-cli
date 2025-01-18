7dbe2f740a881a4a5e12a685fe447eb5270b7633
BastaMasta <56192822+BastaMasta@users.noreply.github.com>
2025-01-18 13:07:37 +0000

    successfully modularized the whole fricking thing, and made it easier to use in the actual library crate

--- binary-testing/src/lib.rs
+++ binary-testing/src/lib.rs
@@ -2,3 +2,7 @@ pub mod bling;
 pub mod templates;
 pub mod macros;
 pub mod constructs;
+pub mod prelude;
+
+// Re-export prelude at crate root
+pub use prelude::*;
---
+++ binary-testing/src/prelude.rs
@@ -0,0 +1,3 @@
+pub use crate::bling::*;
+pub use crate::constructs::{BoxType, BoxAlign};
+pub use crate::boxy;
