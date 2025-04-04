8c62c08078970f6bf7394bb15ce9a8625d2806b4
BastaMasta <56192822+BastaMasta@users.noreply.github.com>
2025-04-04 12:52:07 +0300

    Added a Cargo test

--- binary-testing/src/main.rs
+++ binary-testing/src/main.rs
@@ -1,5 +1,8 @@
+mod tests;
+
 use boxy_cli::prelude::*;
 use terminal_size::terminal_size;
+use std::time::Instant;
 
 
 fn main() {
@@ -14,15 +17,16 @@ fn main() {
     // Post line insert check
     println!("{:?}", box1);
     println!("\nTEXTBOX:");
+    let start = Instant::now();
     box1.display();
+    let duration = start.elapsed();
+    println!("Time elapsed: {:?}", duration);
     let mut box2 = boxy!(type: BoxType::Double, color:"#00ffff");
     box2.add_text_sgmt("Hello There Mateojablij trhwesoiuethj 0piswe hjgtgoise jgtowie3thj q3o-oitujpwiej toiq 0iweeh gt owjtpiewrwh WOKWRHJ JRQWE4IHYNE5R bfg oiwhf apeih aepih aepih aepihetm wf[ohgwlMRF [POWQWRF]] [OJTQEA [OJ]]OJBDGISUDBG SIUGRG OGUFOSIJGOSN SOGUIHSGIORNGR ORIRHGOSJRNGOIJRG OPIFGHRPGNPERIJG ORIRGRPIGNERPGOSJH ", "#ffff");
     box2.add_text_sgmt("Hello Theree", "#ffff");
+    let start1 = Instant::now();
     box2.display();
-    box2.set_align(BoxAlign::Right);
-    box2.display();
-    box2.add_text_line_indx("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur", 1);
-    box2.display();
-
+    let duration1 = start1.elapsed();
+    println!("Time elapsed: {:?}", duration1);
 
 }
